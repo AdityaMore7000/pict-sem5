5be9347d5f19ba2099b98b0c7c2c5e59aa72737f
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2023-08-14 04:34:17 +0000

    remove debug statements

--- LP-1/assign2_A1_assembler/asign2_31072023_31152.cpp
+++ LP-1/assign2_A1_assembler/asign2_31072023_31152.cpp
@@ -7,7 +7,6 @@ using namespace std;
 class pass1{
 public:
 void execute(){
-cout << "At line 1\n";
    map<string, pair<string, string>> opcode;
 
    opcode["STOP"] = {"IS", "00"};
@@ -277,8 +276,6 @@ cout << "At line 1\n";
    }
 
    pout.close();
-
-   cout << "\nProgram Excuted!!" << endl;
 }
 void output_show(){
 ifstream fout;
