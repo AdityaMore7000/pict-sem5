5ce6c8d331e74f57b4275ac339ff49400154ccd7
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2023-09-22 05:02:59 +0000

    start

---
+++ CNSL/subnetting.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of subnets:\n";
+    cin>>n;
+    string defaultSubnetMask="255.255.255.";
+    string defaultNetworkAddress = "192.168.4.";
+    for(int i = 0;i<n;i++){
+        int k = 2;
+        
+    }
+    return 0;
+}
