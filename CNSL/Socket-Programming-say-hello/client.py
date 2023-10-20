3d1a8d85fe41351e2c11ff27c26d02b7fc6a2008
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2023-10-20 04:42:05 +0000

    add screenshots and organize folder structure

--- CNSL/Socket-Programming-say-hello/client.py
+++ CNSL/Socket-Programming-say-hello/client.py
@@ -3,8 +3,8 @@ Socket programming server
 '''
 import socket
 s = socket.socket()
-port = 12346
-s.connect(('127.0.0.1', port))
+PORT = 12346
+s.connect(('127.0.0.1', PORT))
 print (s.recv(1024).decode())
 s.send('Hello server'.encode())
 s.close()
--- CNSL/Socket-Programming-say-hello/server.py
+++ CNSL/Socket-Programming-say-hello/server.py
@@ -4,10 +4,10 @@ Socket programming server
 import socket
 s = socket.socket()
 print ("Socket successfully created")
-ip='127.0.0.1'
-port = 12346
-s.bind((ip, port))
-print ("socket binded to %s"%(port))
+IP='127.0.0.1'
+PORT = 12346
+s.bind((IP, PORT))
+print (f"socket bound to {PORT}")
 s.listen(5)
 print ("socket is listening\n")
 while True:
