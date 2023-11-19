f105e1082336e2c5eec502ea2f84bfb9faa0985f
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2023-11-19 16:05:44 +0530

    Update server.py

--- LP-1/DS/socket-programming/server.py
+++ LP-1/DS/socket-programming/server.py
@@ -2,7 +2,7 @@
 socket programming server
 '''
 import socket
-
+import sys
 HOST = 'localhost'
 PORT = 12345
 connected = True
@@ -14,11 +14,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
-            if data == "exit":
+            if data.decode() == "exit":
+                conn.send("exit".encode())
+                conn.close()
                 connected = False
-                break
+                print(f"Bye")
+                sys.exit(1)
             print('Received:', data.decode())
             MESSAGE = input()
+            if MESSAGE == "exit":
+                conn.send("exit".encode())
+                conn.close()
+                print("exiting")
+                sys.exit(1)
             if connected:
                 conn.send(MESSAGE.encode())
             else:
